81c56f7b11f01e5f57d9a0fe05c18cb13af78b17
Swechha-prob <swechha3424.beai25@chitkara.edu.in>
2026-05-07 10:25:58 +0530

    Pattern of a hollow square with number of rows

--- Patterns/Pattern_Hollow_Square.py
+++ Patterns/Pattern_Hollow_Square.py
@@ -2,7 +2,7 @@ n = int(input("Enter the number of rows:\t"))
 for i in range(1,n+1):
     for j in range(1,n+1):
         if (i==1 or i==n or j==1 or j==n):
-            print(" ",end=" ")
+            print("*",end=" ")
         else:
-            print("*",end=" ") 
+            print(" ",end=" ") 
     print()
